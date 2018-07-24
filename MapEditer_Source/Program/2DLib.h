2c58d677cbf7c0b39c928d268d7590ed6407f2b2
Maeda-Fukuyama <17010518@anabuki-net.ne.jp>
2018-07-24 18:43:31 +0900

    MapEditer_Source

--- MapEditer_Source/Program/2DLib.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIB_H
-#define LIB_H
-#include "CRectangle.h"
-
-void AddY(CRectangle*rect, float i);
-void AddX(CRectangle*rect, float i);
-bool Collision(CRectangle *prect1, CRectangle *prect2);
-
-#endif
