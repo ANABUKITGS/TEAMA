020204cd38954a065f923c622ae6a2268a0fab45
Maeda-Fukuyama <17010518@anabuki-net.ne.jp>
2018-08-21 13:08:36 +0900

    CMapCheckPoint Update

--- GameProject/Game_alpha/GameProgramming/CMapCheckPoint.cpp
+++ GameProject/Game_alpha/GameProgramming/CMapCheckPoint.cpp
@@ -5,14 +5,27 @@ void CMapCheckPoint::Update() {
 	CMapChip::Update();
 }
 
+void CMapCheckPoint::Render(){
+	if (!mCheck)
+		mTexCheckPoint.DrawImage(CHECKPOINT_POS, CHECKPOINT_UV1, 1.0f);
+	else {
+		if (check_ani_count > 6)
+			check_ani_count = 6;
+
+		mTexCheckPoint.DrawImage(CHECKPOINT_POS, CHECKPOINT_UV2, 1.0f);
+
+		check_ani_count_flame++;
+		if (check_ani_count_flame > CHECK_ANI_COUNT_FLAME){
+			check_ani_count++;
+			check_ani_count_flame = 0;
+		}
+	}
+}
+
 bool CMapCheckPoint::Collision(CRectangle *r) {
-	if (CRectangle::Collision(r)){
+	if (CRectangle::Collision(r) && !mCheck){
 		if (r->mTag == ECELLNUM::EPLAYER){
-			mUv[0] = 0;
-			mUv[1] = 64;
-			mUv[2] = 256;
-			mUv[3] = 128;
-			
+			mCheck = true;			
 		}
 		return true;
 	}
--- GameProject/Game_alpha/GameProgramming/CMapCheckPoint.h
+++ GameProject/Game_alpha/GameProgramming/CMapCheckPoint.h
@@ -3,22 +3,33 @@
 
 #include "CMapChip.h"
 
-#define CHECKPOINT_UV1 0 , 64 , 128 , 0
-#define CHECKPOINT_UV2 0 , 64 , 256 , 128
+#define CHECKPOINT_POS	mPosition.x - CELLSIZE, mPosition.x + CELLSIZE, mPosition.y - CELLSIZE / 2, mPosition.y + CELLSIZE * 1.5
+#define CHECKPOINT_UV1	0, 128 , 128 , 0
+#define CHECKPOINT_UV2	128 * check_ani_count, 128 * (check_ani_count + 1), 128, 0
+
+#define CHECK_ANI_COUNT_FLAME	6
 
 class CMapCheckPoint : public CMapChip {
 private:
 	CTexture mTexCheckPoint;
+	int check_ani_count;
+	int check_ani_count_flame;
+	bool mCheck;
 
 public:
-	CMapCheckPoint(const CVector2& pos, const CVector2& scale)
+	CMapCheckPoint(const CVector2& pos)
 		//CMapChipÇ≈èâä˙âª
-		: CMapChip(pos, scale, &mTexCheckPoint, CHECKPOINT_UV1, ECELLNUM::ECHECKPOINT)
+		: CMapChip(pos, CVector2(CELLSIZE / 3, CELLSIZE / 2), NULL, NULL, NULL, NULL, NULL, ECELLNUM::ECHECKPOINT)
 	{
+		mRender = false;
 		mPriority = -10;
 		mTexCheckPoint.Load(".\\Data\\Images\\Map\\Mapcheckpoint.tga");
+		check_ani_count = 0;
+		check_ani_count_flame = 0;
+		mCheck = false;
 	}
 	void Update();
+	void Render();
 	bool Collision(CRectangle *r);	//è’ìÀéûÇÃèàóù
 };
 
