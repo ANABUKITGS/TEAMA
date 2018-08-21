f141ecde371aa271e369e2de75657bb44b1fcd94
Komiyama-Reiji-Fukuyama <17010506@anabuki-net.ne.jp>
2018-08-21 16:29:27 +0900

    Update

--- GameProject/Game_alpha/GameProgramming/CWeapon.h
+++ GameProject/Game_alpha/GameProgramming/CWeapon.h
@@ -4,10 +4,14 @@
 #include "CTaskManager.h"
 #include "CMapJewelry.h"
 
-#define WEAPOM_VELOCITY 12.0f
-#define WEAPON_LIFE 40
+#define WEAPOM_VELOCITY	12.0f
+#define WEAPON_LIFE		40
 
-#define YOYO_UV 
+#define PSTRING_UV_L		mPosition.x, CPlayerT::mpPlayer->mPosition.x, mPosition.y - 10, mPosition.y + 10, 0, 64, 128, 64
+#define PSTRING_UV_R		CPlayerT::mpPlayer->mPosition.x, mPosition.x, mPosition.y - 10, mPosition.y + 10, 0, 64, 128, 64
+
+#define ESTRING_UV_L		mpEWeapon->mPosition.x, mPosition.x, mpEWeapon->mPosition.y - 10, mpEWeapon->mPosition.y + 10, 0, 64, 128, 64
+#define ESTRING_UV_R		mPosition.x, mpEWeapon->mPosition.x, mpEWeapon->mPosition.y - 10, mpEWeapon->mPosition.y + 10, 0, 64, 128, 64
 
 class CWeapon : public CRectangle{
 public:
@@ -28,7 +32,7 @@ public:
 	CWeapon::CWeapon(ECELLNUM tag, const CVector2&position, const bool direction)
 		: CWeapon()
 	{
-		mTexYoyo.Load(".\\Data\\Images\\Player\\YoyoBody.tga");
+		mTexYoyo.Load(".\\Data\\Images\\Player\\Yoyo.tga");
 		SetRectangle(position, CVector2(10, 10), &mTexYoyo);
 		mDirection = direction;
 		mTag = tag;
