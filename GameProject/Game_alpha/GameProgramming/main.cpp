8cb72365b97f520954522612b63ed9d06206f4e4
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2018-08-27 16:07:46 +0900

    FullScreen

--- GameProject/Game_alpha/GameProgramming/main.cpp
+++ GameProject/Game_alpha/GameProgramming/main.cpp
@@ -73,7 +73,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(1280, 720, "GET BACK DIAMOND", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "GET BACK DIAMOND", glfwGetPrimaryMonitor(), NULL);
 	if (!window)
 	{
 		glfwTerminate();
